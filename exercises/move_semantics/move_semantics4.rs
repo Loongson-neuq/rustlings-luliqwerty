687efda57a06bad617d4cb2bab775c672a2d1777
HDQ <3535768174@qq.com>
2024-11-07 15:03:04 +0800

    push

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -7,12 +7,9 @@
 // Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let vec = Vec::new();
-
-    let mut vec = foo(vec0);
+    let mut vec1 = foo();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -22,12 +19,12 @@ fn main() {
 }
 
 // `foo()` does NOT take `vec: Vec<i32>` as argument
-fn foo() -> &mut Vec<i32> {
-    let mut vec = ???
+fn foo() -> Vec<i32> {
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    ???
+    vec
 }
